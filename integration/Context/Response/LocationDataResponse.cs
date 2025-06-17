057a6ac10d4adca21fe686b69aeb13d9b58ad63d
SadGolem <zma2002@mail.ru>
2025-06-17 09:47:38 +0700

    Revert "1"
    This reverts commit 2bca977a0f07283a66fe908cfb7e336b913e93eb.

--- integration/Context/Response/LocationDataResponse.cs
+++ integration/Context/Response/LocationDataResponse.cs
@@ -9,9 +9,13 @@ namespace integration.Context
         [JsonPropertyName("lon")] public decimal lon { get; set; }
         [JsonPropertyName("lat")] public decimal lat { get; set; }
         [JsonPropertyName("address")] public string address { get; set; }
-        [JsonPropertyName("ext_id")] public int? ext_id { get; set; }
+        [JsonPropertyName("ext_id")] public string? ext_id { get; set; }
         
-        [JsonPropertyName("participant")] public Participant participant { get; set; }
+        [JsonIgnore] // Игнорируем при сериализации
+        public bool IsNew => !string.IsNullOrEmpty(ext_id) && 
+                             int.TryParse(ext_id, out int parsed) && 
+                             parsed == 0;
+        [JsonPropertyName("participant")] public Participant? participant { get; set; }
 
         public class Participant()
         {
--- integration/Services/Emitter/EmitterGetterService.cs
+++ integration/Services/Emitter/EmitterGetterService.cs
@@ -78,7 +78,11 @@ public class EmitterGetterService : ServiceGetterBase<EmitterDataResponse>,
                 var emitter = response.FirstOrDefault();
                 emitter.amount = _positionStorage.GetPosOn_ID(id).value;
                 emitter.contractNumber = _positionStorage.GetPosOn_ID(id).number;
-                emitter.location_mt_id = _positionStorage.GetPosOn_ID(id).waste_site.ext_id == null ? 0 : _positionStorage.GetPosOn_ID(id).waste_site.ext_id;
+                emitter.location_mt_id = _positionStorage.GetPosOn_ID(id).waste_site.ext_id == null 
+                    ? 0 
+                    : int.TryParse(_positionStorage.GetPosOn_ID(id).waste_site.ext_id, out int result) 
+                        ? result 
+                        : 0;
                 emitter.executorName = _positionStorage.GetPosOn_ID(id)?.contract?.assignee?.name;
                 emitter.idContract = _positionStorage.GetPosOn_ID(id).contract.id;
                 emitter.contractStatus = _positionStorage.GetPosOn_ID(id).contract?.status?.Name;
