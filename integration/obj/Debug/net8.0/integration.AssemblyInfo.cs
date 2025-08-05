0497eecb501290ee190d49bd4b5581da5d65d9f3
SadGolem <zma2002@mail.ru>
2025-08-05 10:09:15 +0700

    update file nuget project

--- integration/obj/Debug/net8.0/integration.AssemblyInfo.cs
+++ integration/obj/Debug/net8.0/integration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d97250c4909106037975e937a2d69ff03c6e0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55a6b7c80412b11900b590d795ce7b6cba820557")]
 [assembly: System.Reflection.AssemblyProductAttribute("integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
