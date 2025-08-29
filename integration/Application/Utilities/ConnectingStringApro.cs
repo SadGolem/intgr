3c650f9d4ed58cf5c389494f0714b231115db3d9
SadGolem <zma2002@mail.ru>
2025-08-29 08:57:45 +0700

    clear code

--- integration/Application/Utilities/ConnectingStringApro.cs
+++ integration/Application/Utilities/ConnectingStringApro.cs
@@ -14,40 +14,16 @@ public class ConnectingStringApro
         _aproSettings = authSettings.Value.APROconnect;
         InitializeAproConnectSettings(url);
     }
-
-    public string ReplaceStringUtl(string old, string newString)
-    {
-        return _aproConnectSettings.Replace(old, newString);
-    }
-
-    public string ReplaceStringUrlWithoutDate(string old, string newString)
-    {
-        string replaced = _aproConnectSettings.Replace(old, newString); // Заменяем old на newString
-        int index = replaced.IndexOf(newString); // Находим индекс вхождения newString
-        if (index != -1)
-        {
-            return replaced.Substring(0, index + newString.Length); // Обрезаем строку после newString
-        }
-        else
-        {
-            return replaced; // Если newString не найдена, возвращаем исходную строку после замены
-        }
-    }
-
-
     private void InitializeAproConnectSettings(string url)
     {
         // Получаем текущее время в UTC
         DateTimeOffset nowUtc = DateTimeOffset.UtcNow;
-
-        // Вычисляем дату 3 дня назад от текущего времени (начало дня)
-        DateTimeOffset threeDaysAgoUtc = nowUtc.AddDays(-3).Date; // 
-
-        // Форматируем даты в нужный формат (yyyy-MM-dd)
+        
+        DateTimeOffset threeDaysAgoUtc = nowUtc.AddDays(-1).Date;
+        
         string startDate = threeDaysAgoUtc.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         string endDate = nowUtc.Date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-
-
+        
         _aproConnectSettings = _aproSettings.CallbackUrl
             .Replace("token-auth/", $"{url}&datetime_update__gte={startDate}&datetime_update_It={endDate}");
     }
